1524f413c52b66438568c2274494fdc6956c794b
freshfish15 <johnwu8815@gmail.com>
2024-02-21 20:46:44 +0800

    update to base64 and CORS

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 from fastapi import FastAPI, File, UploadFile
 import torch
 from PIL import Image
+import base64
 import open_clip
 import shutil
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 
 app = FastAPI()
@@ -12,13 +15,31 @@ app = FastAPI()
 model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
 tokenizer = open_clip.get_tokenizer('ViT-B-32')
 
+class ImageText(BaseModel):
+    text: str
+    imageBase64: str
+
+
 
 # Define the path where you want to store the uploaded files
 UPLOAD_DIRECTORY = "uploaded_image"
 # Ensure the upload directory exists
 os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -72,3 +93,63 @@ async def upload_image(text: str, file: UploadFile = File(...)):
 
     except Exception as e:
         return str(e)
+
+@app.post("/uploadImageBase64")
+async def upload_image_base64(imagetext: ImageText):
+
+        try:
+            filename = "temp.png"
+            # file_path = os.path.join(UPLOAD_DIRECTORY, filename)
+            image_data_frombase64 = base64.urlsafe_b64decode(imagetext.imageBase64)
+
+            # Open the file in write mode (binary) and save the uploaded file to the filesystem
+
+            #
+            # with open(file_path, "wb") as buffer:
+            #     shutil.copyfileobj(image_data_frombase64, buffer)
+
+            with open('output_image.png', 'wb') as file:
+                file.write(image_data_frombase64)
+
+            file_path = os.path.join(UPLOAD_DIRECTORY, 'output_image.png')
+
+            # Split the input string by comma into a list of strings
+            string_list = imagetext.text.split(',')
+
+            model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+            tokenizer = open_clip.get_tokenizer('ViT-B-32')
+
+            # image_data = image_data_frombase64.read()
+
+            image = preprocess(Image.open(file_path)).unsqueeze(0)
+            # text = tokenizer(["a diagram", "a cat", "a truck"])
+            text = tokenizer(string_list)
+
+            # with torch.no_grad(), torch.cuda.amp.autocast(), torch.cpu.amp.autocast():
+
+            with torch.no_grad():
+                image_features = model.encode_image(image)
+                text_features = model.encode_text(text)
+                image_features /= image_features.norm(dim=-1, keepdim=True)
+                text_features /= text_features.norm(dim=-1, keepdim=True)
+
+                text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+
+            probs_list = text_probs.tolist()
+            decimal_probs_list = [f"{value:.20f}" for value in probs_list[0]]
+
+            combined_dict = dict(zip(string_list, decimal_probs_list))
+
+            # os.remove(file_path)
+            # return("label: ", string_list, "Label probs:", decimal_probs_list)  # prints: [[1., 0., 0.]]
+            return JSONResponse(content=combined_dict)
+
+        except Exception as e:
+            return str(e)
+
+def decode_base64(data):
+    # Check if there is padding
+    missing_padding = len(data) % 4
+    if missing_padding:
+        data += '=' * (4 - missing_padding)  # Add the required padding
+    return base64.b64decode(data)
